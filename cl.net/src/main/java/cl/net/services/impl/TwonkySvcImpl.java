2d27a8c67141ebbdaf363eb01e49f01196eab3ac
grineldosanchez <grineldosanchez@gmail.com>
2013-05-30 22:10:26 +0000

    TwonkySvcImpl

---
+++ cl.net/src/main/java/cl/net/services/impl/TwonkySvcImpl.java
@@ -0,0 +1,16 @@
+package cl.net.services.impl;
+
+import java.io.IOException;
+
+import cl.net.services.ConnectionSvc;
+import cl.net.vo.DirVO;
+import cl.net.vo.SystemVO;
+
+public class TwonkySvcImpl implements ConnectionSvc{
+
+	public void scann(SystemVO systemVO, DirVO dirVO) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
